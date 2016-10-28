586bee837a548cec86bf62c0ab1384da4f66ea8d
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2016-10-28 09:29:13 +0300

    Päivitetty.

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -17,7 +17,13 @@ public class Main {
             port(Integer.valueOf(System.getenv("PORT")));
         }
         
-        Database database = new Database("jdbc:sqlite:keskustelupalsta.db");
+        String jdbcOsoite = "jdbc:sqlite:keskustelupalsta.db";
+        
+        if (System.getenv("DATABASE_URL") != null) {
+            jdbcOsoite = System.getenv("DATABASE_URL");
+        }
+        
+        Database database = new Database(jdbcOsoite);
         database.init();
         
         Spark.staticFileLocation("public");
--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -1,5 +1,6 @@
 package tikape.runko.database;
 
+import java.net.URI;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +11,37 @@ public class Database {
 
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
+        
+        init();
     }
+    
+    
+     public Connection getConnection() throws SQLException {
+        if (this.databaseAddress.contains("postgres")) {
+            try {
+                URI dbUri = new URI(databaseAddress);
 
-    public Connection getConnection() throws SQLException {
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+                return DriverManager.getConnection(dbUrl, username, password);
+            } catch (Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+                t.printStackTrace();
+            }
+        }
+
         return DriverManager.getConnection(databaseAddress);
     }
 
     public void init() {
-        List<String> lauseet = sqliteLauseet();
+        List<String> lauseet = null;
+        if (this.databaseAddress.contains("postgres")) {
+            lauseet = postgreLauseet();
+        } else {
+            lauseet = sqliteLauseet();
+        }
 
         // "try with resources" sulkee resurssin automaattisesti lopuksi
         try (Connection conn = getConnection()) {
@@ -34,18 +58,35 @@ public class Database {
             System.out.println("Error >> " + t.getMessage());
         }
     }
+    
+    private List<String> postgreLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("DROP TABLE Aihealue; DROP TABLE Viestiketju; DROP TABLE Viesti");
+        // heroku käyttää SERIAL-avainsanaa uuden tunnuksen automaattiseen luomiseen
+        lista.add("CREATE TABLE Aihealue (id SERIAL PRIMARY KEY, aiheenNimi varchar(100));");
+        lista.add("CREATE TABLE Viestiketju (id SERIAL PRIMARY KEY, aihealue INTEGER,"
+                + "nimi VARCHAR(100), aika TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+                + "FOREIGN KEY (aihealue) REFERENCES Aihealue(id));");
+        lista.add("CREATE TABLE Viesti (id SERIAL PRIMARY KEY, viestiketju INTEGER,"
+                + "teksti varchar(500), lahettaja VARCHAR(50), lahetysaika DATE DEFAULT(datetime('now', 'localtime')),"
+                + "otsikko VARCHAR(100), FOREIGN KEY (viestiketju) REFERENCES Viestiketju(id));");
+
+        return lista;
+    }
 
     private List<String> sqliteLauseet() {
         ArrayList<String> lista = new ArrayList<>();
 
 //         tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
 //        lista.add("DROP TABLE Aihealue; DROP TABLE Viestiketju; DROP TABLE Viesti;");
-        lista.add("CREATE TABLE Aihealue (id INTEGER PRIMARY KEY, aiheenNimi VARCHAR(100));");
-        lista.add("CREATE TABLE Viestiketju (id INTEGER PRIMARY KEY, aihealue INTEGER, "
-                + "nimi VARCHAR(100), aika TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (aihealue) REFERENCES Aihealue(id));");
-        lista.add("CREATE TABLE Viesti (id INTEGER PRIMARY KEY, viestiketju INTEGER, teksti VARCHAR(500), lahettaja VARCHAR(50), "
-                + "lahetysaika DATE DEFAULT (datetime('now', 'localtime')), "
-                + " otsikko VARCHAR(100), FOREIGN KEY (viestiketju) REFERENCES Viestiketju(id));");
+//        lista.add("CREATE TABLE Aihealue (id INTEGER PRIMARY KEY, aiheenNimi VARCHAR(100));");
+//        lista.add("CREATE TABLE Viestiketju (id INTEGER PRIMARY KEY, aihealue INTEGER, "
+//                + "nimi VARCHAR(100), aika TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (aihealue) REFERENCES Aihealue(id));");
+//        lista.add("CREATE TABLE Viesti (id INTEGER PRIMARY KEY, viestiketju INTEGER, teksti VARCHAR(500), lahettaja VARCHAR(50), "
+//                + "lahetysaika DATE DEFAULT (datetime('now', 'localtime')), "
+//                + " otsikko VARCHAR(100), FOREIGN KEY (viestiketju) REFERENCES Viestiketju(id));");
 //        lista.add("INSERT INTO Aihealue (aiheenNimi) VALUES ('Tietokannat');");
 //        lista.add("INSERT INTO Aihealue (aiheenNimi) VALUES ('Kukkaset ja mehiläiset');");
 //
